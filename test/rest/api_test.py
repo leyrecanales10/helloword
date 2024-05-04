eac1623c67d63b5340daa1cdae75533e5749ae61
leyrecanales10 <leyre.canales483@comunidadunir.net>
2024-05-04 13:50:25 +0200

    Cambiamos error al dividir por 0

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -45,6 +45,17 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "1.0", "ERROR DIVIDE"
         )
 
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/0"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1.0", "ERROR DIVIDE"
+        )
+
     def test_api_multiply(self):
         url = f"{BASE_URL}/calc/multiply/2/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
